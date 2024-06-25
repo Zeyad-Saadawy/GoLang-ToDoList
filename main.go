111e9121232d0f27446151432d6160cb10922dd7
Zeyad Saadawy <zeyadhesham53@gmail.com>
2024-06-25 16:23:17 +0300

    deleting bullet point method

--- main.go
+++ main.go
@@ -53,11 +53,40 @@ func AddBulletPoint(todoID int, content string) {
 	fmt.Printf("Todo with ID %d not found\n", todoID)
 }
 
+// delete a bullet point from a todo list
+func DeleteBulletPoint(todoID int, bulletPointID int) {
+	for i := range Todolists {
+		if Todolists[i].ID == todoID {
+			for j := range Todolists[i].BulletPoints {
+				if Todolists[i].BulletPoints[j].ID == bulletPointID {
+					Todolists[i].BulletPoints = append(Todolists[i].BulletPoints[:j], Todolists[i].BulletPoints[j+1:]...)
+					fmt.Printf("Bullet point with ID %d deleted from todo with ID %d Title %v\n", bulletPointID, todoID, Todolists[i].Title)
+					return
+				}
+			}
+			fmt.Printf("Bullet point with ID %d not found in todo with ID %d\n", bulletPointID, todoID)
+			return
+		}
+	}
+	fmt.Printf("Todo with ID %d not found\n", todoID)
+
+}
 func main() {
 	NewTodo("First Todo")
-	// NewTodo("Second Todo")
 	AddBulletPoint(1, "11111111111111111")
 	AddBulletPoint(1, "222222222222")
+	AddBulletPoint(1, "333333333333")
+	NewTodo("Second Todo")
+	AddBulletPoint(2, "44444444444")
+	AddBulletPoint(2, "555555555")
+	for _, todo := range Todolists {
+		fmt.Printf("Todo ID %d Title %v\n", todo.ID, todo.Title)
+		for _, bulletPoint := range todo.BulletPoints {
+			fmt.Printf("ID %d - Content %v\n", bulletPoint.ID, bulletPoint.Content)
+		}
+	}
+	fmt.Println("-----------------")
+	DeleteBulletPoint(1, 1)
 	for _, todo := range Todolists {
 		fmt.Printf("Todo ID %d Title %v\n", todo.ID, todo.Title)
 		for _, bulletPoint := range todo.BulletPoints {
