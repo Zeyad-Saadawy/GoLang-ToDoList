4cd1054ba53b84a310498ab8dd96f05dc595d51f
Zeyad Saadawy <zeyadhesham53@gmail.com>
2024-06-25 15:10:59 +0300

    create  todos and bullet points for each todo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module To-do-list
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// global variables to generate unique IDs for todos
+var todoIDCounter int
+
+// global variable to keep track of all the todolists
+var Todolists []Todolist
+
+type Todolist struct {
+	ID                   int
+	Title                string
+	BulletPoints         []BulletPoint
+	bulletPointIDCounter int
+}
+
+type BulletPoint struct {
+	ID            int
+	Content       string
+	CompletedTime *time.Time
+}
+
+// Create a new Todo
+func NewTodo(title string) {
+	todoIDCounter++
+	newtodo := Todolist{
+		ID:           todoIDCounter,
+		Title:        title,
+		BulletPoints: []BulletPoint{},
+	}
+	Todolists = append(Todolists, newtodo)
+	fmt.Printf("Todo with ID %d created\n", todoIDCounter)
+}
+
+// add a bullet point to a todo list
+func AddBulletPoint(todoID int, content string) {
+	for i, todo := range Todolists {
+		if todo.ID == todoID {
+			todo.bulletPointIDCounter++
+			newBulletPoint := BulletPoint{
+				ID:      todo.bulletPointIDCounter,
+				Content: content,
+			}
+			Todolists[i].BulletPoints = append(Todolists[i].BulletPoints, newBulletPoint)
+			fmt.Printf("Bullet point with ID %d added to todo with ID %d\n", todo.bulletPointIDCounter, todoID)
+			return
+		}
+	}
+	fmt.Printf("Todo with ID %d not found\n", todoID)
+}
+
+func main() {
+
+}
